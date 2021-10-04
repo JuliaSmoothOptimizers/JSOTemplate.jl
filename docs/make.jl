7f3af87647b52147a5c820bb4889f0b04ae3cccf
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-10-04 19:33:05 +0200

    Add JuliaFormatter and apply it
    Closes #3

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,19 @@
 using JSOTemplate
 using Documenter
 
-DocMeta.setdocmeta!(JSOTemplate, :DocTestSetup, :(using JSOTemplate); recursive=true)
+DocMeta.setdocmeta!(JSOTemplate, :DocTestSetup, :(using JSOTemplate); recursive = true)
 
 makedocs(;
-    modules=[JSOTemplate],
-    authors="Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
-    repo="https://github.com/JuliaSmoothOptimizers/JSOTemplate.jl/blob/{commit}{path}#{line}",
-    sitename="JSOTemplate.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://JuliaSmoothOptimizers.github.io/JSOTemplate.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+  modules = [JSOTemplate],
+  authors = "Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
+  repo = "https://github.com/JuliaSmoothOptimizers/JSOTemplate.jl/blob/{commit}{path}#{line}",
+  sitename = "JSOTemplate.jl",
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://JuliaSmoothOptimizers.github.io/JSOTemplate.jl",
+    assets = String[],
+  ),
+  pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/JuliaSmoothOptimizers/JSOTemplate.jl",
-)
+deploydocs(; repo = "github.com/JuliaSmoothOptimizers/JSOTemplate.jl")
