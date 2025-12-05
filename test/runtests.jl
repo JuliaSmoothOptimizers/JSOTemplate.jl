227d26f2034bb2fd865d928728490a4a74bad118
tmigot <tangi.migot@gmail.com>
2025-12-04 20:27:53 -0500

    Apply all BestieTemplate

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using JSOTemplate
 using Test
 
-@testset "JSOTemplate.jl" begin
-  # Write your tests here.
+#=
+Don't add your tests to runtests.jl. Instead, create files named
+
+    test-title-for-my-test.jl
+
+The file will be automatically included inside a `@testset` with title "Title For My Test".
+=#
+for (root, dirs, files) in walkdir(@__DIR__)
+    for file in files
+        if isnothing(match(r"^test-.*\.jl$", file))
+            continue
+        end
+        title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
+        @testset "$title" begin
+            include(file)
+        end
+    end
 end
---
+++ test/test-basic-test.jl
@@ -0,0 +1,3 @@
+@testset "JSOTemplate.jl" begin
+    @test JSOTemplate.hello_world() == "Hello, World!"
+end
